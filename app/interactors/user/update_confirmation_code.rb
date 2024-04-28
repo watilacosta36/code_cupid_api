de5a306ffc6d2eb5898ae60ec2afe25ba0511251
Wátila <watilafreire26@gmail.com>
2024-04-28 12:07:46 -0300

    refact line code

--- app/interactors/user/update_confirmation_code.rb
+++ app/interactors/user/update_confirmation_code.rb
@@ -6,8 +6,7 @@ module User
 
     def call
       unless context.code.present?
-        raise ActiveModel::MissingAttributeError,
-              I18n.t('activerecord.errors.messages.invalid_code')
+        raise ActiveModel::MissingAttributeError, I18n.t('activerecord.errors.messages.invalid_code')
       end
 
       context.user.update_attribute(:confirmation_code, context.code)
